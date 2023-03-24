c6f932bcf423224c60c7585e8f7cb4511aa225dc
jeff <10823453+jeffmc@users.noreply.github.com>
2023-03-24 07:05:41 -0700

    Added find() + contains(), started remove(), NOT FINISHED

--- types.h
+++ types.h
@@ -13,11 +13,26 @@ struct BNode {
 
 template <typename T>
 class BST {
+    typedef bool ORIENTATION;
+    static const ORIENTATION LEFT = false, RIGHT = true;
     BNode<T> *head = nullptr;
     void delete_tree(BNode<T> *n) {
         if (n->left) delete_tree(n->left);
         if (n->right) delete_tree(n->right);
         delete n;
+    }
+    // u is new parent, v is new child, o is orientation.
+    void shift_nodes(BNode<T> *u, BNode<T> *v, ORIENTATION o)
+    {
+        if (u == nullptr) {
+            head = v;
+        }
+        else if (o == LEFT) {
+            u->left = v;
+        }
+        else {
+            u->right = v;
+        }
     }
 public:
     operator bool() const { return head != nullptr; }
@@ -31,14 +46,14 @@ public:
             BNode<T>* y = nullptr, *x = head;
             do {
                 y = x;
-                if (z->data <= x->data) {
+                if (z->data < x->data) {
                     x = x->left;
                 }
                 else {
                     x = x->right;
                 }
             } while (x != nullptr);
-            if (z->data <= y->data) {
+            if (z->data < y->data) {
                 y->left = z;
             }
             else {
@@ -51,6 +66,42 @@ public:
         if (!head) return;
         delete_tree(head);
         head = nullptr;
+    }
+    // Returns node containing 
+    BNode<T>* find(const T& key) const 
+    {
+        BNode<T> *x = head;
+        while (x != nullptr && x->data != key) {
+            if (key < x->data) {
+                x = x->left;
+            }
+            else {
+                x = x->right;
+            }
+        }
+        return x;
+    }
+    bool contains(const T& key) const {
+        return find(key) != nullptr;
+    }
+    bool remove(const T& key) const 
+    {
+        BNode<T> *y = nullptr, *x = head;
+        ORIENTATION o = LEFT;
+        while (x != nullptr && x->data != key) {
+            y = x;
+            if (key < x->data) {
+                x = x->left;
+                o = LEFT;
+            }
+            else {
+                x = x->right;
+                o = RIGHT;
+            }
+        }
+        if (x == nullptr) return false;
+
+
     }
     template <typename NT>
     friend void print_bst(BST<NT>& bst);
