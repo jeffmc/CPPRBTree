806773c9779e5f0dd40d9c61e45b62aeec75f140
jeffmc <10823453+jeffmc@users.noreply.github.com>
2023-03-27 22:48:11 -0700

    Cleaned up for implementation of RBTree

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+// Jeff McMillan 3-27-23 Red-Black Tree C++
+// This assignment is to implement a red-black tree in C++, 
+// in addition to a CLI for viewing and manipulating the contents of the tree.
+
 #include "types.h"
 #include <iostream>
 #include <cstdio>
@@ -16,13 +20,8 @@ void consolein(char* cstr, std::streamsize n) {
 	#endif
 }
 
-void median_insert(BST<int>& bst, const int* arr, const int count);
-int compint(const void* a, const void* b) {
-	return *(int*)a - *(int*)b;
-}
-
 // Load from space-delimited file of numbers.
-void loadfile(BST<int>& bst) {
+void loadfile(RBTree<int>& rbt) {
 	char filename[32] = { '\0' };
 	FILE* file = nullptr;
 	while (file == nullptr) {
@@ -36,10 +35,8 @@ void loadfile(BST<int>& bst) {
 	printf("File opened successfully!\n");
 	char numbuf[32] = { '\0' };
 	size_t bufpos = 0;
-	int c;
-	int count = 0;
-	int capacity = 128;
-	int* nums = (int*)malloc(capacity * sizeof(int));
+	int c; // current char
+	int read_count = 0, inserted_count = 0;
 	while (true) {
 		c = fgetc(file); // Grab next character from file
 		if (isdigit(c) || c == '+' || c == '-') { // Allow these characters as defined by atoi(const char*) spec.
@@ -50,12 +47,9 @@ void loadfile(BST<int>& bst) {
 		else {
 			if (strlen(numbuf) > 0) {
 				int num = atoi(numbuf);
-				if (count >= capacity) {
-					capacity *= 2;
-					nums = (int*)realloc(nums,capacity*sizeof(int));
-				}
-				nums[count] = num;
-				++count;
+				++read_count;
+				if (rbt.insert(num)) ++inserted_count;
+
 				numbuf[0] = '\0';
 				bufpos = 0;
 			}
@@ -64,18 +58,12 @@ void loadfile(BST<int>& bst) {
 	}
 	assert(strlen(numbuf) == 0);
 	
-	qsort(nums,count,sizeof(int),compint);
-	median_insert(bst,nums,count);
-	printf("Read %i numbers!\n", count);
-	free(nums);
+	printf("Read %i numbers, inserted %i!\n", read_count, inserted_count);
 }
 
 // Manual number entry via console.
-void cmdinput(BST<int>& bst) {
-	const int increment = 100;
-	int capacity = increment;
-	int count = 0;
-	int* nums = (int*)malloc(sizeof(int)*capacity);
+void cmdinput(RBTree<int>& rbt) {
+	int inserted_count = 0;
 	char buf[32] = { '\0' };
 	while (true) {
 		printf("Enter a number (or \"STOP\"): ");
@@ -86,93 +74,30 @@ void cmdinput(BST<int>& bst) {
 		}
 		if (strcmp(buf,"STOP") == 0) break;
 		int num = atoi(buf);
-		if (count >= capacity) {
-			capacity += increment;
-			nums = (int*)realloc(nums,sizeof(int)*capacity);
-		}
-		nums[count] = num;
-		++count;
-	}
-	qsort(nums,count,sizeof(int),compint);
-	median_insert(bst,nums,count);
-	printf("Added %i numbers!\n", count);
-	free(nums);
-}
-
-// Takes sorted array, inserts recursive medians with the goal of making a height-balanced BST.
-void median_insert(BST<int>& bst, const int* arr, const int count) {
-	struct Block { int pos, len; };
-	struct Node { Block b; Node* n; };
-
-	Node* head = new Node{Block{0,count},nullptr};
-	Node* tail = head;
-
-	while (head != nullptr) {
-		// Pop head
-		const Block b = head->b;
-		// printf("On block: %i, %i\n", b.pos, b.len);
-		if (head == tail) {
-			delete head;
-			head = nullptr;
-			tail = nullptr;
+		if (rbt.insert(num)) {
+			++inserted_count;
 		}
 		else {
-			Node* oh = head;
-			head = head->n;
-			delete oh;
-		}
-
-		int rel_med_idx = b.len / 2;
-		int abs_med_idx = b.pos + rel_med_idx;
-
-		printf("%i ", arr[abs_med_idx]);
-		bst.insert(arr[abs_med_idx]);
-		
-		if (rel_med_idx > 0) {
-			Node* n = new Node{Block{b.pos,rel_med_idx},nullptr};
-			if (tail) {
-				tail->n = n;
-				tail = tail->n;
-			}
-			else {
-				head = n;
-				tail = n;
-			}
-		}
-		int rblk_sz = rel_med_idx;
-		if (b.len%2==0) --rblk_sz;
-		if (rblk_sz > 0) {
-			Node* n = new Node{Block{abs_med_idx+1,rblk_sz},nullptr};
-			if (tail) {
-				tail->n = n;
-				tail = tail->n;
-			}
-			else {
-				head = n;
-				tail = n;
-			}
+			printf("Tree already contains %i, try another\n", num);
 		}
 	}
-	printf("\n");
+	printf("Added %i numbers!\n", inserted_count);
 }
 
 // Randomly generate numbers and add to heap.
-void randomGen(BST<int>& bst, int count, int max) {
+void randomGen(RBTree<int>& rbt, int count, int max) {
 	srand(time(NULL));
 
-	int* nums = new int[count];
-
-	printf("Generating and adding %i numbers...\n", count);
+	int inserted_count = 0;
+	printf("Generating %i numbers...\n", count);
 	for (int i=0;i<count;i++) {
-		nums[i] = rand() % max;
+		if (rbt.insert(rand() % max)) ++inserted_count;
 	}
-	qsort(nums,count,sizeof(int),compint);
-	median_insert(bst,nums,count);
-	delete[] nums;
+	printf("Inserted %i numbers...\n", inserted_count);
 }
 
 // https://stackoverflow.com/questions/36802354/print-binary-tree-in-a-pretty-way-using-c
-void print_bnode(BNode<int> *node, const char* prefix, bool isLeft) {
+void print_rbnode(RBNode<int> *node, const char* prefix, bool isLeft) {
     if( node == nullptr ) {
         return;
     }
@@ -187,49 +112,45 @@ void print_bnode(BNode<int> *node, const char* prefix, bool isLeft) {
     strcat(npref, isLeft ? "┃  " : "   ");
 
     // enter the next tree level - left and right branch
-    print_bnode(node->left, npref, node->right == nullptr ? false : true);
-    print_bnode(node->right, npref, false);
+    print_rbnode(node->left, npref, node->right == nullptr ? false : true);
+    print_rbnode(node->right, npref, false);
 
     delete[] npref;
 }
 
 template <typename T>
-void print_bst(const BST<T>& bst) {
+void print_rbt(const RBTree<T>& rbt) {
     assert(false);
     printf("Unspec template!\n");
 }
 template <>
-void print_bst(const BST<int>& bst) {
-    if (bst.head) 
-    {
-        print_bnode(bst.head,"",false);
-    }
-    else {
-        printf("Empty tree!\n");
-    }
+void print_rbt(const RBTree<int>& rbt) {
+	print_rbnode(rbt.head,"",false);
 }
 
-void search_bst(const BST<int>& bst) {
+void search_rbt(const RBTree<int>& rbt) {
 	char buf[32] = { '\0' };
 	printf("Number to search for: ");
 	consolein(buf,32);
 	int key = atoi(buf);
 	printf("Searching for %i...\n", key);
-	bool found = bst.contains(key);
-	printf("%s %i\n", found ? "Found" : "Did not find", key); 
+	assert(false);
+	// bool found = bst.contains(key);
+	// printf("%s %i\n", found ? "Found" : "Did not find", key); 
 }
-void delete_bst(BST<int>& bst) {
+void delete_rbt(RBTree<int>& rbt) {
 	char buf[32] = { '\0' };
 	printf("Number to remove: ");
 	consolein(buf,32);
 	int key = atoi(buf);
 	printf("Searching for %i...\n", key);
-	bool removed = bst.remove(key);
-	printf("%s %i\n", removed ? "Removed" : "Did not find", key); 
+	assert(false);
+	// bool removed = bst.remove(key);
+	// printf("%s %i\n", removed ? "Removed" : "Did not find", key); 
 }
 
 int main() {
-	BST<int> bst;
+	RBTree<int> rbt;
 	const char* const HELP_STR = "Commands:\n"
 		"	HELP - displays this list of commands.\n"
 		"	MANUAL - manually add numbers to the BST.\n"
@@ -242,7 +163,6 @@ int main() {
         "	QUIT - end program.\n";
 
 	printf(HELP_STR);
-
 	char buf[32] = { '\0' };
 	while (true) {
 		printf("> ");
@@ -251,27 +171,27 @@ int main() {
 			printf(HELP_STR);
 		}
 		else if (strcmp(buf,"MANUAL") == 0) {
-			cmdinput(bst);
+			cmdinput(rbt);
 		}
 		else if (strcmp(buf,"FILE") == 0) {
-			loadfile(bst);
+			loadfile(rbt);
 		}
 		else if (strcmp(buf,"RANDOM") == 0) {
-			randomGen(bst,25,100);
+			randomGen(rbt,25,100);
 		}
 		else if (strcmp(buf,"CLEAR") == 0) {
-			bst.clear();
+			rbt.clear();
 			printf("Cleared BST of all nodes!\n");
 		}
 		else if (strcmp(buf,"TREE") == 0) {
 			printf("\n TREE: \n");
-			print_bst(bst);
+			print_rbt(rbt);
 		}
 		else if (strcmp(buf,"SEARCH") == 0) {
-			search_bst(bst);
+			search_rbt(rbt);
 		}
 		else if (strcmp(buf,"DELETE") == 0) {
-			delete_bst(bst);
+			delete_rbt(rbt);
 		}
 		else if (strcmp(buf,"QUIT") == 0) {
 			printf("Exiting!\n");
--- types.h
+++ types.h
@@ -2,179 +2,43 @@
 #include <cstdlib>
 
 template <typename NT>
-struct BNode {
+struct RBNode {
+    typedef bool col_t;
+    static constexpr col_t RED = false, BLACK = true; 
+
+    col_t col;
     NT data;
     BNode *left = nullptr, *right = nullptr;
+
     BNode(const NT& o) : data(NT(o)) {}
     BNode() {}
     ~BNode() {};
 };
 
 template <typename T>
-class BST {
-    typedef bool ORIENTATION;
-    static const ORIENTATION LEFT = false, RIGHT = true;
-    BNode<T> *head = nullptr;
-    void delete_tree(BNode<T> *n) {
-        if (n->left) delete_tree(n->left);
-        if (n->right) delete_tree(n->right);
-        delete n;
-    }
-    void shift_nodes(BNode<T>* up, BNode<T> *u, BNode<T> *v) {
-        if (up == nullptr) {
-            assert(u == head);
-            head = v;
-        } else if (u == up->left) {
-            up->left = v;
-        } else {
-            up->right = v;
-        }
-    } 
-    BNode<T>* bst_minimum(BNode<T> *x) {
-        while (x->left != nullptr) x = x->left;
-        return x;
-    } 
-    BNode<T>* bst_minimum(BNode<T> *x, BNode<T>* &p) {
-        while (x->left != nullptr) {
-            p = x;
-            x = x->left;
-        }
-        return x;
-    }
-    BNode<T>* bst_maximum(BNode<T> *x) {
-        while (x->right != nullptr) x = x->right;
-        return x;
-    }
-    // Parent list, parent list size
-    BNode<T>* successor(BNode<T>** pl, int plsz, BNode<T> *x) {
-        if (x->right != nullptr) {
-            return bst_minimum(x->right);
-        }
-        BNode<T> *y = x->parent;
-        while (y != nullptr && x == y->right) {
-            x = y;
-            y = y->parent;
-        }
-        return y;
-    }
-    // Parent list, parent list size
-    BNode<T>* predecessor(BNode<T>** pl, int plsz, BNode<T> *x) {
-        if (x->left != nullptr) {
-            return bst_maximum(x->left);
-        }
-        BNode<T> *y = x->parent;
-        while (y != nullptr && x == y->left) {
-            x = y;
-            y = y->parent;
-        }
-        return y;
-    }
-    void bst_delete(BNode<T>* dp, BNode<T> *d) {
-        if (d->left == nullptr) {
-            shift_nodes(dp, d, d->right);
-        } else if (d->right == nullptr) {
-            shift_nodes(dp, d, d->left);
-        } else {
-            BNode<T> *ep = nullptr;
-            BNode<T> *e = bst_minimum(d->right, ep);
-            if (e != d->right) { // if successor is not d's immediate child
-                shift_nodes(ep, e, e->right);
-                e->right = d->right;
-            }
-            shift_nodes(dp,d,e);
-            e->left = d->left;
-        }
-        delete d;
+class RBTree {
+    RBNode<T> *head = nullptr;
+    delete_tree(RBNode<T> *x) {
+        if (x == nullptr) return;
+        delete_tree(x->left);
+        delete_tree(x->right);
+        delete x;
     }
 public:
     operator bool() const { return head != nullptr; }
     template <typename ...Args> 
-    // Returns true if not already contained (doesn't duplicate), insert the key at a fitting space in the tree.
+    // Returns true if successfully inserted, will return false if data is already contained (doesn't duplicate).
     bool insert(const T& o) {
-        BNode<T>* z = new BNode<T>(o);
-        if (head == nullptr) {
-            head = z;
-        }
-        else {
-            BNode<T>* y = nullptr, *x = head;
-            do {
-                y = x;
-                if (z->data == x->data) {
-                    delete z;
-                    return true;
-                }
-                else if (z->data < x->data) {
-                    x = x->left;
-                }
-                else {
-                    x = x->right;
-                }
-            } while (x != nullptr);
-            if (z->data < y->data) {
-                y->left = z;
-            }
-            else {
-                y->right = z;
-            }
-        }
+        assert(false);
         return false;
     }
     // Delete all nodes
     void clear() 
     {
-        if (!head) return;
         delete_tree(head);
         head = nullptr;
     }
-    // Returns (first) node equal to specified key
-    BNode<T>* find(const T& key) 
-    {
-        BNode<T> *x = head;
-        while (x != nullptr && x->data != key) {
-            if (key < x->data) {
-                x = x->left;
-            }
-            else {
-                x = x->right;
-            }
-        }
-        return x;
-    }
-    // Returns (first) node equal to specified key
-    const BNode<T>* find(const T& key) const 
-    {
-        BNode<T> *x = head;
-        while (x != nullptr && x->data != key) {
-            if (key < x->data) {
-                x = x->left;
-            }
-            else {
-                x = x->right;
-            }
-        }
-        return x;
-    }
-    // Return true if BST contains key (at least once).
-    bool contains(const T& key) const {
-        return find(key) != nullptr;
-    }
-    // Return true if key was removed from BST (once).
-    bool remove(const T& key)  
-    {
-        BNode<T> *xp = nullptr, *x = head;
-        while (x != nullptr && x->data != key) {
-            xp = x;
-            if (key < x->data) {
-                x = x->left;
-            }
-            else {
-                x = x->right;
-            }
-        }
-        if (x == nullptr) return false;
-        bst_delete(xp,x);
-        return true;
-    }
+
     template <typename NT>
-    friend void print_bst(const BST<NT>& bst);
+    friend void print_rbt(const RBTree<NT>&);
 };
