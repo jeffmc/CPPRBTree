a022699b8bf0f02fe6bed4992996ef0e344e9887
Jeff McMillan <10823453+jeffmc@users.noreply.github.com>
2023-05-09 08:14:01 -0700

    Implemented real debug

--- debug.h
+++ debug.h
@@ -1,4 +1,14 @@
 #pragma once
 
+#include <cstdio>
+#include <cassert>
+
+#define DBG 1
+
+#if DBG
+#define DBGLOG(note) printf("\x1b[32m[DEBUG]: %s:%i    %s: \"%s\"\n\x1b[39m", __FILE__, __LINE__, __PRETTY_FUNCTION__, note);
+#define DBGTRACE()   printf("\x1b[32m[DEBUG]: %s:%i    %s\n\x1b[39m", __FILE__, __LINE__, __PRETTY_FUNCTION__);
+#else
+#define DBGLOG(note)
 #define DBGTRACE()
-#define DBGLOG(msg)
+#endif
